44a9fce9d82b751834f02b7002db2fd724b61723
aliffajriadi <aliffajriadi@gmail.com>
2025-05-03 18:25:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,9 @@
         const stat = document.getElementById('status');
         try {
             const response = await fetch('https://3f18-43-209-20-233.ngrok-free.app/api/test');
-            const data = await response.json();
-            stat.innerHTML = `<p class="text-white bg-green-500 p-2 rounded">BOT ${data.message}</p>`;
+            if (response.ok) {
+                stat.innerHTML = `<p class="text-white bg-green-500 p-2 rounded">BOT ${data.message}</p>`;
+            };
         } catch (error) {
             console.error("Terjadi error saat fetch:", error); // tambahkan log
             stat.innerHTML = `<p class="text-white bg-red-500 p-2 rounded">BOT Offline</p>`;
